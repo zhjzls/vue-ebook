14370e4758139995f7a0039b43496e51e40eebff
zhjzls <zhjzls@163.com>
2019-07-25 11:01:15 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <div id="read">
+        <span class="iconfont icon-brightness"></span>
+    </div>
+  </div>
+</template>
+<script>
+import Epub from 'epubjs'
+global.epub = Epub
+
+export default {
+  // name: 'home',
+  // mounted () {
+  //   this.book = new Epub('/java8.epub')
+  //   console.log(this.book)
+  //   console.log(15, Epub)
+  //   this.book.renderTo('read', {
+  //     width: window.innerWidth,
+  //     height: window.innerHeight
+  //   }).display()
+  // }
+}
+</script>
+<style scoped>
+  .icon-brightness{
+    color:red;
+    font-size: 24px;
+  }
+</style>
